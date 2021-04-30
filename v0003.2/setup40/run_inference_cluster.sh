a21f915702ce1f8ecdf07cab46a9e9418f63566d
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2021-04-30 14:54:41 -0400

    adjust to updates in CNNectome for inference

--- v0003.2/setup40/run_inference_cluster.sh
+++ v0003.2/setup40/run_inference_cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 WD=$(pwd)
-WDTL=$(basename $(pwd))
+SETUPDIR=${PWD#${PWD%/*/*/*}/}
+SETUP=${SETUPDIR////_}
 DP=$(basename $3)
 RUNSCRIPT=${@}
 echo $RUNSCRIPT
@@ -9,21 +10,21 @@ echo "Preparing inference"
 singularity exec \
             --nv \
             --containall \
-            -B /groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
+            -B /scratch/$USER:/tmp,/groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
             --pwd $WD \
             /groups/saalfeld/home/heinrichl/singularity-builds/cnnectome.sif \
-            /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u unet_inference_template.py prepare $RUNSCRIPT"
+            /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u inference_config.py prepare $RUNSCRIPT"
 echo "Running inference split over $1 jobs"
 
 for i in $(seq 0 `expr $1 - 1`);
 do
-    bsub -J "$3-$4-$WDTL-$i" -P cosem -n 2 -gpu "num=1" -q gpu_any -o "$DP-$4-$WDTL-$i-output.log" \
-    -e "$DP-$4-$WDTL-$i-error.log" -We 300 \
+    bsub -J "$3-$4-$SETUP-$i" -P cosem -n 2 -gpu "num=1" -q gpu_short -o "$DP-$4-$SETUP-$i-output.log" \
+    -e "$DP-$4-$SETUP-$i-error.log" -W 240 \
     singularity exec \
                 --nv \
                 --containall \
-                -B /groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
+                -B /scratch/$USER:/tmp,/groups/saalfeld,/nrs/saalfeld,/groups/turaga,/groups/cosem/cosem,/nrs/cosem \
                 --pwd $WD \
                 /groups/saalfeld/home/heinrichl/singularity-builds/cnnectome.sif \
-                /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002;python -u unet_inference_template.py inference $i ${*:2} ";
+                /bin/bash --norc -c "export OMP_NUM_THREADS=1; umask 0002; python -u inference_config.py inference $i ${*:2} ";
 done;
